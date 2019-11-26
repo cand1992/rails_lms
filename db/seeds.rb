fd4eb86b71dc85bc98dc53ba7419bc77e2e0f840
Chris Anderson <chris.anderson955@gmail.com>
2019-11-25 20:55:39 -0700

    started enrollments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+role = [ 'teacher', 'ta', 'student', 'admin' ]
+
+10.times do
+	course = Course.create(
+		name: Faker::Food.sushi
+	)
+
+	5.times do
+		user = User.create(
+			first_name: Faker::FunnyName.name,
+			last_name: Faker::Dessert.topping
+		)
+
+		Enrollment.create(
+			role: role.sample,
+			course_id: course.id,
+			user_id: user.id
+		)
+	end
+end
+
+puts 'Data is seeded'
